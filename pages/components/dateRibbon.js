9a1356e0c346e128881e8f3e55d2dd094d328a6b
blemo <blessedemoefe1122334455@gmail.com>
2022-04-06 03:26:36 +0100

    commit

--- pages/components/dateRibbon.js
+++ pages/components/dateRibbon.js
@@ -15,4 +15,4 @@ export default function DateRibbon() {
             </ul>
         </div>
     );
-};
+}
--- pages/components/languageTranslator.js
+++ pages/components/languageTranslator.js
@@ -1,4 +1,4 @@
-import styles from '../../styles/Home.module.css';
+
 
 export default function LanguageTranslator() {
     return (
